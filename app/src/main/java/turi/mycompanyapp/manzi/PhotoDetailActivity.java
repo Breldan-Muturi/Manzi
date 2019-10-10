27747584b8d315f1ee370657c0febb59da35d0ea
Turi-byte <breldanmuturi001@gmail.com>
2019-10-10 11:13:07 +0300

    Use the String resources

--- app/src/main/java/turi/mycompanyapp/manzi/PhotoDetailActivity.java
+++ app/src/main/java/turi/mycompanyapp/manzi/PhotoDetailActivity.java
@@ -1,16 +1,9 @@
 package turi.mycompanyapp.manzi;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.squareup.picasso.Picasso;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,11 +19,14 @@ public class PhotoDetailActivity extends BaseActivity {
         Intent intent = getIntent();
         Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
         if(photo != null){
-            TextView photoTitle = (TextView) findViewById(R.id.photo_title);
-            photoTitle.setText("Title: " + photo.getTitle());
+            TextView photoTitle = findViewById(R.id.photo_title);
+            Resources resources = getResources();
+            String text = resources.getString(R.string.photo_title_text, photo.getTitle());
+            photoTitle.setText(text);
+//            photoTitle.setText("Title: " + photo.getTitle());
 
-            TextView photoTags = (TextView) findViewById(R.id.photo_tags);
-            photoTags.setText("Tags: " + photo.getTags());
+            TextView photoTags = findViewById(R.id.photo_tags);
+//            photoTags.setText("Tags: " + photo.getTags());
 
             TextView photoAuthor = (TextView) findViewById(R.id.photo_author);
             photoAuthor.setText(photo.getAuthor());
