61b6195c857ae4922e5fdd08fc1492a3ee03237e
Turi-byte <breldanmuturi001@gmail.com>
2019-10-10 13:47:24 +0300

    SearchManager

--- app/src/main/java/turi/mycompanyapp/manzi/SearchActivity.java
+++ app/src/main/java/turi/mycompanyapp/manzi/SearchActivity.java
@@ -1,5 +1,8 @@
 package turi.mycompanyapp.manzi;
 
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,24 +12,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 public class SearchActivity extends BaseActivity {
 
+    private static final String TAG = "SearchActivity";
     private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: starts");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
         activateToolbar(true);
+        Log.d(TAG, "onCreate: ends");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu: starts");
         getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        mSearchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        mSearchView.setSearchableInfo(searchableInfo);
+        Log.d(TAG, "onCreateOptionsMenu: " + getComponentName().toString());
+        Log.d(TAG, "onCreateOptionsMenu: hint is " +mSearchView.getQueryHint());
+        Log.d(TAG, "onCreateOptionsMenu: searchable info is "+searchableInfo.toString());
+
+        mSearchView.setIconified(true);
+        Log.d(TAG, "onCreateOptionsMenu: returned" + true   );
         return true;
     }
 }
